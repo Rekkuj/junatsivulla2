0ac2b3992c2fe97c0b67896026921ea46a62ee47
unknown <elmo.visuri@gmail.com>
2018-06-26 13:39:01 +0300

    new cookies

---
+++ JS/cookies.js
@@ -0,0 +1,6 @@
+var myCookies = {};
+
+function saveCookies() {
+    myCookies["_fromSt"] = document.getElementById("fromSt").value;
+    myCookies["_toSt"] = document.getElementById("toSt").value;
+}
