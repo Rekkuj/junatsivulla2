f13d727d754deda0c319ac9c2bcd6ade26c93eb7
Reija Jokinen <reija.jokinen@kolumbus.fi>
2018-06-27 10:33:50 +0300

    Masteriin logot ym.

--- JS/junat.js
+++ JS/junat.js
@@ -2,6 +2,7 @@ var listOfTrains = document.getElementById("trainlist");
 var fromSt = document.getElementById("fromSt");
 var toSt = document.getElementById("toSt");
 var timeSettings = {hour: "2-digit", minute: '2-digit', hour12: false};
+var dateSettings = {weekday: 'short', day: 'numeric', month: 'numeric'};
 
 
 var req = new XMLHttpRequest();
@@ -71,6 +72,7 @@ req2.onreadystatechange = function () {
             var trainTable = JSON.parse(req2.responseText);
             console.log(trainTable);
             addToList(trainTable);
+            console.log(distance(stationInfo[fromSt.value],stationInfo[toSt.value]));
         } else {
             alert("Lataaminen epäonnistui.");
         }
@@ -92,48 +94,84 @@ function addToList(table) {
 
     for (var i = 0; i < lengthOrMax; i++) {
         var train = table[i];
-        var departureTime = new Date(train.timeTableRows[0].scheduledTime);
-        var arrivalTime = new Date(train.timeTableRows[train.timeTableRows.length - 1].scheduledTime);
+
+        var indexOfDeparture;
+        var indexOfArriving;
+
+        for (var j = 0; j < train.timeTableRows.length; j++) {
+            if (train.timeTableRows[j].stationShortCode === stationInfo[fromSt.value].shortCode){
+                if (j===0) {
+                    indexOfDeparture = j;
+                } else{
+                    indexOfDeparture = j+1;
+                }
+            }
+            if (train.timeTableRows[j].stationShortCode === stationInfo[toSt.value].shortCode) {
+                indexOfArriving = j;
+                break;
+            }
+        }
+
+
+        var departureDate = new Date(train.departureDate);
+        var departureTime = new Date(train.timeTableRows[indexOfDeparture].scheduledTime);
+        var arrivalTime = new Date(train.timeTableRows[indexOfArriving].scheduledTime);
 
         var idouter = "train" + i;
 
         $("<li></li>", {id: idouter})
-            .append(train.trainType + train.trainNumber
-                + ", Lähtöaika: " + departureTime.toLocaleTimeString('fi', timeSettings)
-                + ", Saapumisaika: " + arrivalTime.toLocaleTimeString('fi', timeSettings))
+            .append("<strong>" + fromSt.value + " - " + toSt.value + ": " + train.trainType + train.trainNumber + "</strong>"
+                + "<br>Lähtö: " + departureDate.toLocaleDateString('fi', dateSettings)
+                + " klo. " + departureTime.toLocaleTimeString('fi', timeSettings)
+                + "<br>Perillä: " + arrivalTime.toLocaleTimeString('fi', timeSettings))
+            .click(function () {
+                $(this.lastChild).toggleClass("hide");
+            })
             .appendTo(listOfTrains);
 
-        printTableRow(idouter, train, i);
+        printTableRow(idouter, train, i, indexOfDeparture);
 
     }
 }
 
-function printTableRow(idouter, train, i) {
+function printTableRow(idouter, train, i, startingIndex) {
     var trainId = train.trainNumber + train.trainType + i;
-    $("<ul></ul>", {id: trainId}).appendTo(document.getElementById(idouter));
-    outer:
-        for (var j = 1; j < train.timeTableRows.length; j = j + 2) {
+    $("<ul></ul>", {id: trainId}).addClass("hide").appendTo(document.getElementById(idouter));
 
-            if (stationShortCodes.indexOf(train.timeTableRows[j].stationShortCode) < 0) {
-                continue outer;
-            }
-            if (train.timeTableRows[j].trainStopping == false) {
-                continue outer;
-            }
-            var stationCode = train.timeTableRows[j].stationShortCode;
-            var index = stationShortCodes.indexOf(stationCode);
-            var departureTime = new Date(train.timeTableRows[j].scheduledTime);
 
+    for (var j = startingIndex; j < train.timeTableRows.length; j = j + 2) {
+
+        if (stationShortCodes.indexOf(train.timeTableRows[j].stationShortCode) < 0) {
+            continue;
+        }
+        if (train.timeTableRows[j].trainStopping === false) {
+            continue;
+        }
+
+        var stationCode = train.timeTableRows[j].stationShortCode;
+        var index = stationShortCodes.indexOf(stationCode);
+        var departureTime = new Date(train.timeTableRows[j].scheduledTime);
+
+        if (train.timeTableRows[j].stationShortCode === stationInfo[toSt.value].shortCode) {
             $("<li></li>").append(stationNames[index]
                 + ": "
                 + departureTime.toLocaleTimeString('fi', timeSettings))
                 .appendTo(document.getElementById(trainId));
-            if (train.timeTableRows[j].stationShortCode == stationInfo[toSt.value].shortCode) {
-                break outer;
-            }
+            break;
         }
 
+        var arrivalTime = new Date(train.timeTableRows[j+1].scheduledTime);
+        var stopLength = parseInt((arrivalTime-departureTime)/(1000*60));
+        var stopLengthWord;
+        if (stopLength == 1){
+            stopLengthWord = "minuutti.";
+        } else {
+            stopLengthWord = "minuuttia.";
+        }
+        $("<li></li>").append(stationNames[index]
+            + ": "
+            + departureTime.toLocaleTimeString('fi', timeSettings)
+            + " , pysähdys " + stopLength + stopLengthWord)
+            .appendTo(document.getElementById(trainId));
+    }
 }
-
-
-
