b412941a71afe090482e9f2847e302a841bd8a9d
Reija Jokinen <reija.jokinen@kolumbus.fi>
2018-06-28 08:48:05 +0300

    features lisätty omaan fileen

---
+++ JS/features.js
@@ -0,0 +1,12 @@
+/*
+ * Features
+ * @author Reija Jokinen (http://github.com/Rekkuj)
+ */
+
+function scaleOn() {
+    this.className='scaleOn';
+}
+
+function scaleOff() {
+    this.className='scales'
+}
