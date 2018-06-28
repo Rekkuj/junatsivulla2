5c5c744b010eac5b537fae8ca9dcef3f20bc5dd6
unknown <elmo.visuri@gmail.com>
2018-06-28 10:52:27 +0300

    login working..or..

--- JS/login.js
+++ JS/login.js
@@ -15,14 +15,17 @@ function setNewUserToMap() {
 
     // for (var key of loginMap.keys()) {
     //     console.log(key);
-    //     if(key.contains(user)) {
+    //     if(key.has(user)) {
     //         console.log("name is already used")
     //         return alert("Username is alreade used"),clearNameAndPass();
     //     }
     //
+    //     loginMap.set(user.value, pass.value);
+    //     console.log("User and pass saved");
+    //
     // }
 
-    if(loginMap.has(user)) {
+    if(loginMap.has(user.value)) {
         console.log("User is already used");
         return window.confirm("Käyttäjänimi on jo käytössä, ole hyvä ja vaihda nimi.")
     }
@@ -42,11 +45,35 @@ function clearNameAndPass() {
 function login() {
     var user = document.getElementById("user");
     var pass = document.getElementById("pass");
+    var kw = loginMap.get(user.value);
+    console.log(kw);
+    console.log(pass.value);
 
-    for (var [key, value] of loginMap) {
-        if(key.contains(user)) {
-            console.log(user.getName());
-        }
+    // var kwc = kw.caesar();
+
+    if (kw === pass.value) {
+        console.log("Login ok");
+        welcome();
+    } else {
+        console.log("Something went wrooong");
+        alert("Tarkista nimi ja salasana");
     }
 
+    // for (var [key, value] of loginMap) {
+    //     if(key.contains(user)) {
+    //         console.log(user.getValue());
+    //         if(this.user(value).equals(pass,pass.value)) {
+    //             console.log("User and pass match");
+    //             welcome();
+    //         } else {
+    //             return "Tarkista nimi ja salasana";
+    //         }
+    //     return "Tarkista nimi ja salasana";
+    //     }
+    // }
+
+}
+
+function welcome() {
+    return alert("Tervetuloa" + user);
 }
